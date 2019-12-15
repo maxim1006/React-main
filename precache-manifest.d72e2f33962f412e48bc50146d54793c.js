2bd78837a5bb7c751ad907396ba1f8aae4e3934e
max <maximprosv@gmail.com>
2019-12-15 05:48:40 +0300

    Updates

---
+++ precache-manifest.d72e2f33962f412e48bc50146d54793c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5015170648f415b398b3",
+    "url": "/react-main/static/js/0.50151706.chunk.js"
+  },
+  {
+    "revision": "84baddf7a75982bc4360",
+    "url": "/react-main/static/css/main.648d445e.chunk.css"
+  },
+  {
+    "revision": "84baddf7a75982bc4360",
+    "url": "/react-main/static/js/main.84baddf7.chunk.js"
+  },
+  {
+    "revision": "0f0b7bd25ef48af07ff2",
+    "url": "/react-main/static/css/2.2ffc7210.chunk.css"
+  },
+  {
+    "revision": "0f0b7bd25ef48af07ff2",
+    "url": "/react-main/static/js/2.0f0b7bd2.chunk.js"
+  },
+  {
+    "revision": "6ef2dc3a23d50601830d",
+    "url": "/react-main/static/js/3.6ef2dc3a.chunk.js"
+  },
+  {
+    "revision": "14a3416b093dd242d1a5",
+    "url": "/react-main/static/css/4.e032ff83.chunk.css"
+  },
+  {
+    "revision": "14a3416b093dd242d1a5",
+    "url": "/react-main/static/js/4.14a3416b.chunk.js"
+  },
+  {
+    "revision": "c816e18843088b6a4257",
+    "url": "/react-main/static/js/5.c816e188.chunk.js"
+  },
+  {
+    "revision": "3ddbb691eaf395c57c13",
+    "url": "/react-main/static/js/6.3ddbb691.chunk.js"
+  },
+  {
+    "revision": "0f1b09420359c014849c",
+    "url": "/react-main/static/js/7.0f1b0942.chunk.js"
+  },
+  {
+    "revision": "cf5fe9d0590ea8e3b6c2",
+    "url": "/react-main/static/js/runtime~main.cf5fe9d0.js"
+  },
+  {
+    "revision": "1deb630b566ae5c5f731a5bcfb63d4ac",
+    "url": "/react-main/index.html"
+  }
+];
---
+++ static/js/6.3ddbb691.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{338:function(n,e,t){"use strict";t.r(e);var u=t(0),o=t.n(u);e.default=function(){return o.a.createElement("h4",null,"RouterLazyRoute")}}}]);
+//# sourceMappingURL=6.3ddbb691.chunk.js.map
