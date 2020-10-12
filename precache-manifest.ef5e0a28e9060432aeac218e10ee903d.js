c35bdb553dd668a05a6c440e0ab2612e143fba83
Max Maximov <maxim.maximov@netcracker.com>
2020-10-13 01:09:31 +0300

    Updates

---
+++ precache-manifest.ef5e0a28e9060432aeac218e10ee903d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb63fb6e002ff0f6ca8adfd86fa7d803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef71aa93a519590fc4b2",
+    "url": "/static/css/10.b5ed4241.chunk.css"
+  },
+  {
+    "revision": "bf62d95b4c8532735a02",
+    "url": "/static/css/11.92d979c2.chunk.css"
+  },
+  {
+    "revision": "3005c668deb28d42f022",
+    "url": "/static/css/12.d4dadc4a.chunk.css"
+  },
+  {
+    "revision": "02e6e874fce4f5f2e2ad",
+    "url": "/static/css/14.4180b2a4.chunk.css"
+  },
+  {
+    "revision": "c736df6479664a579618",
+    "url": "/static/css/5.9917c0f7.chunk.css"
+  },
+  {
+    "revision": "55da0097778dd10d9bd7",
+    "url": "/static/css/6.2926c816.chunk.css"
+  },
+  {
+    "revision": "07c1910de68eceeae413",
+    "url": "/static/css/8.8fdba651.chunk.css"
+  },
+  {
+    "revision": "8c8d22527a028c08711b",
+    "url": "/static/css/9.1b151b86.chunk.css"
+  },
+  {
+    "revision": "6283904ae0b8c9ca5c08",
+    "url": "/static/css/main.bbd93f4d.chunk.css"
+  },
+  {
+    "revision": "8fe7122455a8a6af5285",
+    "url": "/static/js/0.19a2338c.chunk.js"
+  },
+  {
+    "revision": "2ea6b1a655f550170a4b",
+    "url": "/static/js/1.09d2f193.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/1.09d2f193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef71aa93a519590fc4b2",
+    "url": "/static/js/10.f7c56fb9.chunk.js"
+  },
+  {
+    "revision": "bf62d95b4c8532735a02",
+    "url": "/static/js/11.e69eea45.chunk.js"
+  },
+  {
+    "revision": "3005c668deb28d42f022",
+    "url": "/static/js/12.cd1ad382.chunk.js"
+  },
+  {
+    "revision": "e54501dc7c809a5c51e6",
+    "url": "/static/js/13.6bc9feb9.chunk.js"
+  },
+  {
+    "revision": "02e6e874fce4f5f2e2ad",
+    "url": "/static/js/14.d6244909.chunk.js"
+  },
+  {
+    "revision": "be76d1adeb3d62ee76a0",
+    "url": "/static/js/15.edbaaaac.chunk.js"
+  },
+  {
+    "revision": "1d826b51481713a72854",
+    "url": "/static/js/16.e668fbb9.chunk.js"
+  },
+  {
+    "revision": "680bd5cf37e588c3cb77",
+    "url": "/static/js/17.10d9816e.chunk.js"
+  },
+  {
+    "revision": "c79857e0a92983ddf220",
+    "url": "/static/js/18.63ca31c6.chunk.js"
+  },
+  {
+    "revision": "b52f1f8bdf55ec3e3da2",
+    "url": "/static/js/19.fbb9f04e.chunk.js"
+  },
+  {
+    "revision": "3572f14e0ef018292875",
+    "url": "/static/js/20.6e3f14be.chunk.js"
+  },
+  {
+    "revision": "cfd2d8480cbeb11bd6a9",
+    "url": "/static/js/21.e6d428b4.chunk.js"
+  },
+  {
+    "revision": "1faf8e80301b31f9dc83",
+    "url": "/static/js/4.61f2e256.chunk.js"
+  },
+  {
+    "revision": "4b6ff87288029a7b288110f445284443",
+    "url": "/static/js/4.61f2e256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c736df6479664a579618",
+    "url": "/static/js/5.edc160de.chunk.js"
+  },
+  {
+    "revision": "55da0097778dd10d9bd7",
+    "url": "/static/js/6.60c2c014.chunk.js"
+  },
+  {
+    "revision": "c86c17c09ca8d4650f8a",
+    "url": "/static/js/7.7f2f1360.chunk.js"
+  },
+  {
+    "revision": "07c1910de68eceeae413",
+    "url": "/static/js/8.b7cc3c00.chunk.js"
+  },
+  {
+    "revision": "8c8d22527a028c08711b",
+    "url": "/static/js/9.78c02328.chunk.js"
+  },
+  {
+    "revision": "6283904ae0b8c9ca5c08",
+    "url": "/static/js/main.80e84e83.chunk.js"
+  },
+  {
+    "revision": "e7acbb8c3086d6447116",
+    "url": "/static/js/runtime-main.8fee386a.js"
+  },
+  {
+    "revision": "4c5c4961d2a36d0f83a43b80826eb4ff",
+    "url": "/static/media/crown.4c5c4961.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
--- static/js/23.c2de917b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-main"]=this["webpackJsonpreact-main"]||[]).push([[23],{423:function(a){a.exports=JSON.parse("[]")}}]);
-//# sourceMappingURL=23.c2de917b.chunk.js.map
